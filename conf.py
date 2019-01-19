98ecc93839df2dbb06fae2be63e78b28e5cb8db6
Dustin Spicuzza <dustin@virtualroadside.com>
2019-01-19 11:49:26 -0500

    Black

--- conf.py
+++ conf.py
@@ -27,12 +27,12 @@ sys.path.insert(0, abspath(join(dirname(__file__))))
 # -- RTD configuration ------------------------------------------------
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 # This is used for linking and such so we link to the thing we're building
-rtd_version = os.environ.get('READTHEDOCS_VERSION', 'latest')
-if rtd_version not in ['stable', 'latest']:
-    rtd_version = 'stable'
+rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+if rtd_version not in ["stable", "latest"]:
+    rtd_version = "stable"
 
 # -- General configuration ------------------------------------------------
 
@@ -40,34 +40,40 @@ if rtd_version not in ['stable', 'latest']:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'RobotPy'
-copyright = '2014-2017, RobotPy development team'
+project = "RobotPy"
+copyright = "2014-2017, RobotPy development team"
 
-# TODO: detect this at 
-rtd_version = 'latest'
+# TODO: detect this at
+rtd_version = "latest"
 
 intersphinx_mapping = {
-    'pyfrc': ('http://pyfrc.readthedocs.io/en/%s/' % rtd_version, None),
-    'networktables': ('http://pynetworktables.readthedocs.io/en/%s/' % rtd_version, None),
-    'wpilib': ('http://robotpy-wpilib.readthedocs.io/en/%s/' % rtd_version, None),
-    'robotpy_ext': ('http://robotpy-wpilib-utilities.readthedocs.io/en/%s/' % rtd_version, None),
-    'cscore': ('http://robotpy-cscore.readthedocs.io/en/%s/' % rtd_version, None),
+    "pyfrc": ("http://pyfrc.readthedocs.io/en/%s/" % rtd_version, None),
+    "networktables": (
+        "http://pynetworktables.readthedocs.io/en/%s/" % rtd_version,
+        None,
+    ),
+    "wpilib": ("http://robotpy-wpilib.readthedocs.io/en/%s/" % rtd_version, None),
+    "robotpy_ext": (
+        "http://robotpy-wpilib-utilities.readthedocs.io/en/%s/" % rtd_version,
+        None,
+    ),
+    "cscore": ("http://robotpy-cscore.readthedocs.io/en/%s/" % rtd_version, None),
 }
 
 # The version info for the project you're documenting, acts as replacement for
@@ -75,34 +81,35 @@ intersphinx_mapping = {
 # built documents.
 #
 # The short X.Y version.
-version = '2017'
+version = "2017"
 # The full version, including alpha/beta/rc tags.
 release = version
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = "default"
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
+
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
-    html_theme = 'default'
+    html_theme = "default"
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'RobotPy'
+htmlhelp_basename = "RobotPy"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -113,8 +120,13 @@ latex_elements = {}
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'RobotPy.tex', 'RobotPy Documentation',
-   'RobotPy development team', 'manual'),
+    (
+        "index",
+        "RobotPy.tex",
+        "RobotPy Documentation",
+        "RobotPy development team",
+        "manual",
+    )
 ]
 
 # -- Options for Texinfo output -------------------------------------------
@@ -123,62 +135,72 @@ latex_documents = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'RobotPy', 'RobotPy Documentation',
-   'RobotPy development team', 'RobotPy', 'One line description of project.',
-   'Miscellaneous'),
+    (
+        "index",
+        "RobotPy",
+        "RobotPy Documentation",
+        "RobotPy development team",
+        "RobotPy",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
 
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = 'RobotPy'
-epub_author = 'RobotPy development team'
-epub_publisher = 'RobotPy development team'
-epub_copyright = '2014-2016, RobotPy development team'
+epub_title = "RobotPy"
+epub_author = "RobotPy development team"
+epub_publisher = "RobotPy development team"
+epub_copyright = "2014-2016, RobotPy development team"
 
 # A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
+epub_exclude_files = ["search.html"]
 
 # -- Custom Document processing ----------------------------------------------
 
 import gensidebar
-gensidebar.generate_sidebar(globals(), 'robotpy')
+
+gensidebar.generate_sidebar(globals(), "robotpy")
 
 
 import sphinx.addnodes
 import docutils.nodes
 
+
 def process_child(node):
-    '''This function changes class references to not have the
-       intermediate module name by hacking at the doctree'''
-    
+    """This function changes class references to not have the
+       intermediate module name by hacking at the doctree"""
+
     # Edit descriptions to be nicer
     if isinstance(node, sphinx.addnodes.desc_addname):
         if len(node.children) == 1:
             child = node.children[0]
             text = child.astext()
-            if text.startswith('wpilib.') and text.endswith('.'):
+            if text.startswith("wpilib.") and text.endswith("."):
                 # remove the last element
-                text = '.'.join(text.split('.')[:-2]) + '.'
+                text = ".".join(text.split(".")[:-2]) + "."
                 node.children[0] = docutils.nodes.Text(text)
-                
+
     # Edit literals to be nicer
     elif isinstance(node, docutils.nodes.literal):
         child = node.children[0]
         text = child.astext()
-        
+
         # Remove the imported module name
-        if text.startswith('wpilib.'):
-            stext = text.split('.')
-            text = '.'.join(stext[:-2] + [stext[-1]])
+        if text.startswith("wpilib."):
+            stext = text.split(".")
+            text = ".".join(stext[:-2] + [stext[-1]])
             node.children[0] = docutils.nodes.Text(text)
-    
+
     for child in node.children:
         process_child(child)
+
 
 def doctree_read(app, doctree):
     for child in doctree.children:
         process_child(child)
 
+
 def setup(app):
-    app.connect('doctree-read', doctree_read)
+    app.connect("doctree-read", doctree_read)
